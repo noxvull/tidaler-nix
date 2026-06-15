37a682d79664ff6a321f16adcb26aa50080ef023
Mikaela Aleinikova <mikaela.aleinikova@proton.me>
2026-06-15 19:23:37 +0200

    added nix support

---
+++ default.nix
@@ -0,0 +1,50 @@
+{
+  lib,
+  pythonPackages,
+  enableGui ? false,
+}:
+pythonPackages.buildPythonPackage rec {
+  pname = "tidaler";
+  version = "0.1.7";
+  format = "pyproject";
+
+  src = pythonPackages.fetchPypi {
+    inherit pname version;
+    sha256 = "sha256-UM9F07EXdN1G0+iFCX+mjgK0quOkJ93mE88YNj4TjII=";
+  };
+
+  build-system = with pythonPackages; [
+    poetry-core
+  ];
+
+  dependencies = with pythonPackages;
+    [
+      requests
+      mutagen
+      dataclasses-json
+      pathvalidate
+      m3u8
+      coloredlogs
+      rich
+      toml
+      typer
+      tidalapi
+      python-ffmpeg
+      pycryptodome
+      ansi2html
+    ]
+    ++ (lib.optionals enableGui [pyside6]);
+
+  pyprojet = true;
+  doCheck = false;
+  # because versions of "typer" and "requests" are higher, than specified in pyproject it won't build
+  # skiping check. doesn't cause any issue to have a newer version in this case
+  # https://discourse.nixos.org/t/packaging-python-for-nixpkgs-w-dependency-from-github/40415
+  dontCheckRuntimeDeps = true;
+
+  meta = with lib; {
+    description = "TIDAL Downloader (fork of tidal-dl-ng)";
+    homepage = "https://github.com/maya-doshi/tidaler";
+    license = licenses.agpl3Only;
+  };
+}
---
+++ flake.nix
@@ -0,0 +1,65 @@
+{
+  description = "TIDAL Downloader (fork of tidal-dl-ng)";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    (flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+
+      callTidaler = {enableGui ? false}:
+        pkgs.callPackage ./default.nix {
+          pythonPackages = pkgs.python312Packages;
+          inherit enableGui;
+        };
+    in {
+      packages = {
+        default = callTidaler {enableGui = false;};
+        tidaler-gui = callTidaler {enableGui = true;};
+      };
+
+      devShells.default = pkgs.mkShell {
+        packages = [
+          pkgs.poetry
+          pkgs.ffmpeg
+          (pkgs.python312.withPackages (ps: (callTidaler {enableGui = true;}).dependencies))
+        ];
+      };
+    }))
+    // {
+      homeManagerModules.default = {
+        config,
+        lib,
+        pkgs,
+        ...
+      }: let
+        cfg = config.programs.tidaler;
+      in {
+        options.programs.tidaler = {
+          enable = lib.mkEnableOption "TIDAL Downloader CLI/GUI framework";
+          gui = lib.mkOption {
+            type = lib.types.bool;
+            default = false;
+            description = "Whether to build and install with PySide6 GUI capabilities.";
+          };
+        };
+
+        config = lib.mkIf cfg.enable {
+          home.packages = [
+            (
+              if cfg.gui
+              then self.packages.${pkgs.system}.tidaler-gui
+              else self.packages.${pkgs.system}.default
+            )
+          ];
+        };
+      };
+    };
+}
